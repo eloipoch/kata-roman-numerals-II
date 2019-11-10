4220d5862730865c5950c4383787aa275fecb123
Eloi Poch <eloi.poch@gmail.com>
2019-11-10 18:21:50 +0100

    refactor

--- src/main/kotlin/RomanNumerals.kt
+++ src/main/kotlin/RomanNumerals.kt
@@ -23,13 +23,9 @@ val transformations: Transformations = listOf(
     1000 to "M"
 )
 
-fun transform(number: Arabic) = transformations.foldRight(number to "", transform()).roman
+fun transform(number: Arabic) = transformations.foldRight(number to "", ::transform).roman
 
-private fun transform(): (Transformation, Transformation) -> Transformation =
-    { transformation, result -> transform(result, transformation) }
-
-private tailrec fun transform(result: Transformation, transformation: Transformation): Transformation =
+private tailrec fun transform(transformation: Transformation, result: Transformation): Transformation =
     if (result.arabic < transformation.arabic) result else transform(
-        result.arabic - transformation.arabic to result.roman + transformation.roman,
-        transformation
+        transformation, result.arabic - transformation.arabic to result.roman + transformation.roman
     )
