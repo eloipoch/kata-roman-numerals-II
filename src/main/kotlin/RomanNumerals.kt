e8bb2c71152a83996a40143f004ee5313106bec0
Eloi Poch <eloi.poch@gmail.com>
2019-11-10 17:01:44 +0100

    refactor

--- src/main/kotlin/RomanNumerals.kt
+++ src/main/kotlin/RomanNumerals.kt
@@ -1,30 +1,30 @@
-fun transform(arabic: Int): String {
-    val transformations = listOf(
-        1 to "I",
-        4 to "IV",
-        5 to "V",
-        9 to "IX",
-        10 to "X",
-        40 to "XL",
-        50 to "L",
-        90 to "XC",
-        100 to "C",
-        400 to "CD",
-        500 to "D",
-        900 to "CM",
-        1000 to "M"
-    )
+typealias Arabic = Int
+typealias Roman = String
+typealias Transformation = Pair<Arabic, Roman>
+typealias Transformations = List<Transformation>
 
-    return transform(arabic, transformations)
-}
+val transformations: Transformations = listOf(
+    1 to "I",
+    4 to "IV",
+    5 to "V",
+    9 to "IX",
+    10 to "X",
+    40 to "XL",
+    50 to "L",
+    90 to "XC",
+    100 to "C",
+    400 to "CD",
+    500 to "D",
+    900 to "CM",
+    1000 to "M"
+)
 
-private fun transform(arabic: Int, transformations: List<Pair<Int, String>>) =
-    transformations.foldRight(arabic to "") { (value, into), (arabic, roman) ->
-        var result = arabic to roman
+fun transform(number: Arabic) = transformations.foldRight(number to "") { (value, into), (arabic, roman) ->
+    var result = arabic to roman
 
-        while (result.first >= value) {
-            result = result.first - value to result.second + into
-        }
+    while (result.first >= value) {
+        result = result.first - value to result.second + into
+    }
 
-        result
-    }.second
+    result
+}.second
